503d64d24b0933f00fcbf262ce26a2a5c995aa25
brian21a <Sourcream@csu.fullerton.edu>
2019-09-21 14:57:20 -0700

    making a random rectangle

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,19 @@
 // Random Rectangle
+#include <iostream>
+#include <ctime>
+ int main()
+ {
+
+unsigned seed = time(0) ;
+srand(seed);
+int MaxWidth = 40;
+int MaxLength = 3;
+MaxLength = rand() % MaxWidth + 1;
+std::string eq ;
+eq.assign(MaxLength,'#') ;
+
+std::cout << eq << std::endl;
+std::cout << eq << std::endl;
+std::cout << eq << std::endl;
+    return 0;
+  }
